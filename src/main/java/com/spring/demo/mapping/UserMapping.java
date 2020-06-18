a9fa57050ba9126df1aab4e2b926a3f69d8ee8da
dcd654321 <dcd654321@163.com>
2020-06-18 23:14:11 +0800

    bransh测试

--- src/main/java/com/spring/demo/mapping/UserMapping.java
+++ src/main/java/com/spring/demo/mapping/UserMapping.java
@@ -8,8 +8,7 @@ import org.springframework.stereotype.Service;
 public interface UserMapping {
     @Select("select * from user")
     public User selectAll();
-    
-    @Select("select * from user where a.name = #{name}")
-    public User selectUserByName(String name);
+
+
 
 }
