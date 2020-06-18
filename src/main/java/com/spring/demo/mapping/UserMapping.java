7f87ca66f896d175a25ff656831dc419555730aa
dcd654321 <dcd654321@163.com>
2020-06-18 23:04:44 +0800

    二次修改测试

--- src/main/java/com/spring/demo/mapping/UserMapping.java
+++ src/main/java/com/spring/demo/mapping/UserMapping.java
@@ -1,4 +1,11 @@
 package com.spring.demo.mapping;
 
+import com.spring.demo.domain.User;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface UserMapping {
+    @Select("select * from user")
+    public User selectAll();
 }
